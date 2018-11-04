de1ea9e1c99e00287cf83304a4ae35abd290a843
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2018-11-03 21:23:49 -0300

    changed coursera-unsub to more functional

--- coursera-unsub.js
+++ coursera-unsub.js
@@ -1,5 +1,2 @@
-checkboxes = document.querySelectorAll("[type='checkbox']"); 
-checked = Array.from(checkboxes).filter(x=> x.checked); 
-for(let item of checked){     
-    item.click() 
-}
+Array.from(document.querySelectorAll("[type='checkbox']")).filter(x=> x.checked).forEach(x=>x.click());
+Array.from(document.querySelectorAll("button")).filter(x=> x.textContent == "Save").forEach(x=>x.click());
